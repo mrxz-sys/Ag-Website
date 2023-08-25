a370cda3a21857c7bb84523015c52db799758d31
mrxz-sys <60146430+mrxz-sys@users.noreply.github.com>
2023-08-25 18:16:53 +0100

    Update ag.js

--- ag.js
+++ ag.js
@@ -33,7 +33,7 @@ $(function(){
     var carouselEl = $('.carousel');
     var carouselItems = carouselEl.find('.item');
     carouselEl.carousel({
-      interval: 1000
+      interval: 15000
     }).on('slid.bs.carousel', function (event) {
         console.log(carouselItems.siblings('.active').index());
         if(carouselItems.siblings('.active').index()=="0"){//////////First Slide
