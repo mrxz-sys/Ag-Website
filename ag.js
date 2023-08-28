d1c44f11fb382ec12ffeb8fe21551586c5ef12e7
mrxz-sys <60146430+mrxz-sys@users.noreply.github.com>
2023-08-28 18:11:20 +0100

    Update ag.js

--- ag.js
+++ ag.js
@@ -71,18 +71,20 @@ function applyStylesToEnvp(element, styles) {
 
 // Define the styles for each element based on screen width
 var env_svg_st = {
-  between: 'margin-left: 100%;'
+  between: 'margin-left: 75%;'
 };
 
 var env_svg_open_st = {
-  between: 'margin-left: 100%;'
+  between: 'margin-left: 75%;'
 };
 var tel_svg_st = {
   between: 'margin-left: 10%;'
 };
+
 var env_svg=document.getElementById("env_svg");
 var env_svg_open=document.getElementById("env_svg_open");
 var tel_svg=document.getElementById("tel_svg");
+var geo_svg=document.getElementById("geo_svg");
 
 function visibility() {
 env_svg.setAttribute("class","bi bi-envelope hidden");
@@ -97,6 +99,7 @@ env_svg_open.setAttribute("class","bi bi-envelope hidden");
 applyStylesToEnvp(env_svg, env_svg_st);
 applyStylesToEnvp(env_svg_open, env_svg_open_st);
 applyStylesToEnvp(tel_svg, tel_svg_st);
+
 //Today's Date Updated (Today's day only)
 var options = { weekday: 'long', };
 const today = new Date();
