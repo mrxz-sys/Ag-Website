9145bf6ec93e143486b9b923695116dd0c997588
mrxz-sys <60146430+mrxz-sys@users.noreply.github.com>
2023-08-28 17:18:56 +0100

    Update ag.js

--- ag.js
+++ ag.js
@@ -71,11 +71,11 @@ function applyStylesToEnvp(element, styles) {
 
 // Define the styles for each element based on screen width
 var env_svg_st = {
-  between: 'margin-left: 50%;'
+  between: 'margin-left: 80%;'
 };
 
 var env_svg_open_st = {
-  between: 'margin-left: 50%;'
+  between: 'margin-left: 80%;'
 };
 var env_svg=document.getElementById("env_svg");
 var env_svg_open=document.getElementById("env_svg_open");
